5f4083b3c18f8970f5525c9e587331dcd6908f8d
gerleim <gerleim@gmail.com>
2026-02-22 00:57:19 +0100

    Add ContextMenuStyle class used by WpfPlotMenu

---
+++ src/ScottPlot5/ScottPlot5 Controls/ScottPlot.WPF/ContextMenuStyle.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ScottPlot.WPF;
+
+public class ContextMenuStyle
+{
+    public Thickness? MenuPadding { get; set; }
+    public Thickness? ItemPadding { get; set; }
+    public double? ItemFontSize { get; set; }
+    public double? ItemMinHeight { get; set; }
+    public double? ItemMinWidth { get; set; }
+}
